8a0bc394e67c8cd0eafea8e11e571fca3ed3d471
Andrey Popp <8mayday@gmail.com>
2017-08-09 12:35:26 +0300

    feat(packument): add API for fetching packuments

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 module.exports = {
   extract: require('./extract'),
   manifest: require('./manifest'),
+  packument: require('./packument'),
   prefetch: require('./prefetch'),
   clearMemoized: require('./lib/fetch').clearMemoized
 }
